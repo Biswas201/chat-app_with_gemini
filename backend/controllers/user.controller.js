f343f255bb0522d51eedfb88a2b2495c0c4c5ce8
anurup <anurupbiswas143@gmail.com>
2024-12-29 13:01:34 +0530

    login, authentication & install frontend

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,6 +1,8 @@
 import userModel from "../models/user.model.js";
+import redisClient from "../services/redis.services.js";
 import * as userService from "../services/user.service.js";
 import { validationResult } from "express-validator";
+
 
 export const createUserController = async (req, res) => {
     const errors = validationResult(req);
@@ -16,5 +18,53 @@ export const createUserController = async (req, res) => {
         res.status(201).json({user, token});
     } catch (error) {
         res.status(400).send(error.message);
+    }
+}
+
+export const loginUserController = async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+        const { email, password } = req.body;
+        const user = await userModel.findOne({ email }).select("+password");
+        if (!user) {
+            res.status(401).json({ message: "Invalid credentials" });
+        }
+
+        const isMatch = await user.isValidPassword(password);
+        if (!isMatch) {
+            res.status(401).json({ message: "Invalid credentials" });
+        }
+
+        const token = await user.generateJWT();
+
+        res.status(200).json({user, token});
+
+    } catch(err){
+        res.status(400).send(err.message);
+    }
+}
+
+export const profileController = async (req, res) => {
+    console.log(req.user);
+
+    res.status(200).json({ user: req.user });    
+}
+
+export const logoutController = async (req, res) => {
+    try {
+        
+        const token = req.cookies.token || req.headers.authorization.split(" ")[1];
+
+        redisClient.set(token, "logout", "EX", 60 * 60 * 24);
+
+        res.status(200).json({ message: "Logout successful" });
+
+    } catch (err) {
+        console.log(err);
+        res.status(400).send(err.message);
     }
 }
--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -29,16 +29,18 @@ const userSchema = new mongoose.Schema({
 });
 
 userSchema.statics.hashPassword = async function (password) {
-    return await bcrypt.hash(password, 10);
-}
+  return await bcrypt.hash(password, 10);
+};
 
 userSchema.methods.isValidPassword = async function (password) {
-    return await bcrypt.compare(password, this.password);
-}
+  return await bcrypt.compare(password, this.password);
+};
 
 userSchema.methods.generateJWT = function () {
-    return jwt.sign({ email: this.email }, process.env.JWT_SECRET);
-}
+  return jwt.sign({ email: this.email }, process.env.JWT_SECRET, {
+    expiresIn: "24h",
+  });
+};
 
 const User = mongoose.model("User", userSchema);
 
